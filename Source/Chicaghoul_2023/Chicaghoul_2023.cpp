8d96d086c1c445a43667cfcbe95e63bdd5bacf03
Brad Patterson <bradstar879@yahoo.com>
2023-10-09 23:51:57 -0500

    Started ingredients

---
+++ Source/Chicaghoul_2023/Chicaghoul_2023.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Chicaghoul_2023.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Chicaghoul_2023, "Chicaghoul_2023" );
